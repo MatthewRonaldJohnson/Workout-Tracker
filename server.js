c314ec11d52e35fe13a037f61f3bc9d0d906b8bd
Matthew Johnson <MatthewRonaldJohnson@gmail.com>
2021-04-16 09:00:49 -0500

    added mongo atlas

--- server.js
+++ server.js
@@ -11,10 +11,12 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
 
-mongoose.connect("mongodb://localhost/workout", {
-    useNewUrlParser: true,
-    useFindAndModify: false
-});
+mongoose.connect(
+    process.env.MONGODB_URI || "mongodb://localhost/workout",
+    {
+        useNewUrlParser: true,
+        useFindAndModify: false
+    });
 
 app.use(routes)
 app.listen(PORT, () => console.log('App Listening on Port: ' + PORT))
